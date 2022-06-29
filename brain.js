1ad7b5088b9bcf4adea6b22556e55a1f9ea348dd
Pratyush <86536877+pratyush4932@users.noreply.github.com>
2022-06-29 23:36:44 +0530

    Update brain.js

--- brain.js
+++ brain.js
@@ -1,3 +1,4 @@
+
 require('./config')
 const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType } = require('@adiwajshing/baileys')
 const fs = require('fs')
