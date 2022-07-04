f47279808629881d342db89074f66d4610a4d31a
Toshi さん <91844428+Toshiro-sama@users.noreply.github.com>
2022-07-05 00:26:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,8 +16,6 @@ global.port = process.env.PORT || 8080;
   (global.author = process.env.AUTHOR || "ARUS"),
   (global.sessionName = "arus"),
   (global.name = process.env.NAME || "Mizuhara"),
-  (global.menu =
-    "🎫 Mizuhara's Command Liste* 🎫\n\n💻 *Dev*\n❐ ```update, broadcast, ban, unban, chatid, session, join```\n\n🔮 *General*\n❐ ```help, delete, info, rank, profile```\n\n💠 *Fun*\n❐ ```ship, advice, quote, leaderboard, react, fact, bot```\n\n🔈 *Media*\n❐ ```yta, ytv, play, gify, yts, lyrics```\n\n👑 *Moderation*\n❐ ```demote, remove, ping, promote, deact, act, invite, group, seticon, setdesc```\n\n💮 *Weeb*\n❐ ```anime, manga, charecter, haigusha, neko, waifu, pokemon```\n\n🖇 *Utils*\n❐ ```google, image, meme, subreddit, tourl, img, iguser, gif, sticker, take, pinterest, carbon```\n\n🗃️ *Note: *Calls and Dm will lead you to ban*"),
   (global.thumb = fs.readFileSync("./src/info.jpeg"));
 
 let file = require.resolve(__filename);
